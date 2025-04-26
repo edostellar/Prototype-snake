a9c2a1354562916a5dae5ab2ffad7f3e662be2f4
Isaac Mejia <tunuevocorreo@gmail.com>
2025-04-26 13:35:14 -0400

    repor 6

--- constantes.py
+++ constantes.py
@@ -1,5 +1,20 @@
+import pygame
+
+#TAMAÑO DE LA VENTANA
 WIDTH = 1000
 HEIGHT = 1000
-WHITE = ('white')
-BLACK = ('black')
+
+#COLORES ESTABLECIDOS
+WHITE = ( [200, 200, 200] )
+YELLOW = ( [255, 255, 0] )
+BLUE = ( [0, 0, 255] )
+RED = ( [255, 0, 0] )
+BLACK = ( [0, 0, 0] )
+GREEN = ( [0, 255, 0] )
+
 BlockSize = 50
+
+#IMAGENES
+ICON = pygame.image.load("Sprites/game_icon.png")
+BG = pygame.image.load("Sprites/sky.png")
+BG = pygame.transform.scale(BG, (1000, 1000))
--- firstgame.py
+++ firstgame.py
@@ -1,31 +1,40 @@
-import pygame
-import constantes
+    
+try:
+    import pygame #inicia el programa
+    import constantes #importa las varibles
+    print("All Libraries imported successfully \n")
+except :
+    print("Error while importing modules and libraries")
+
 
 pygame.init()
 
 screen = pygame.display.set_mode((constantes.WIDTH, constantes.HEIGHT))
 clock = pygame.time.Clock()
 
-pygame.display.set_caption('Lorem Ipsum')
-
-bg_img = pygame.image.load('img/sky.png')
+#Titulo del juego
+pygame.display.set_caption('Snake Game Prototype')
+pygame.display.set_icon(constantes.ICON)
 
 def draw_grid():
     for x in range(0, constantes.WIDTH, constantes.BlockSize):
         for y in range(0, constantes.HEIGHT, constantes.BlockSize):
-            rect = pygame.Rect(x, y, constantes.BlockSize)
+            rect = pygame.Rect(x, y, constantes.BlockSize, constantes.BlockSize)
             pygame.draw.rect(screen, constantes.WHITE, rect, 1)
 
 run = True
 while run:
-    
-    screen.blit(bg_img, (0, 0))
-    screen.fill((0, 0, 0))  # Fill the background to clear the previous frame
+
+    screen.blit(constantes.BG, [0, 0])
     draw_grid()
+
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+            pygame.display.update()
 clock.tick(60)
 pygame.quit()
+print ("Exited with no errors")
 
